3f66e01c57c24f0e577ee5f0245c92b97d8e7a8b
Brandon Reynolds <breynolds@uttyler.edu>
2026-05-21 15:34:28 -0500

    Update vite.config.ts
    revise base path
    add path for main and voting

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change 'bip-vote' to match your GitHub repository name
-// e.g. if repo is github.com/etxlaunchpad/bip-vote → base: '/bip-vote/'
 export default defineConfig({
   plugins: [react()],
-  base: '/bip-vote/',
+  base: '/bip-voting/',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        vote: 'vote.html',
+      }
+    }
   },
 })
